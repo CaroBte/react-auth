b7d350b576c9eb5e9ea4a7528aff443d8f04c646
CaroBte <karito0721@hotmail.com>
2023-01-30 17:33:19 -0500

    Login, Logout, Profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Login from './components/Login';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
       <Login />
+      <Profile />
+      <Logout />
     </div>
   );
 }
