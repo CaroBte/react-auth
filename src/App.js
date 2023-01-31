83428040308682c9a86cd42b436a76c156e92427
CaroBte <karito0721@hotmail.com>
2023-01-30 20:25:30 -0500

    useAuth0

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
+  const { isAuthenticated } = useAuth0()
+
+
   return (
     <div className="App">
       <h1>Hello world!</h1>
-      <Login />
-      <Profile />
-      <Logout />
+      {isAuthenticated ? <>
+        <Profile />
+        <Logout />
+      </> : <Login />}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ root.render(
     <Auth0Provider
       domain='carobte-dev.us.auth0.com'
       clientId='AB6SVGWqHYVDWb6AgTvRYVfmlLxZHZSO'
-      redirectUri={window.location.origin}
+      authorizationParams={{ redirect_uri: window.location.origin }}
     >
       <App />
     </Auth0Provider>
